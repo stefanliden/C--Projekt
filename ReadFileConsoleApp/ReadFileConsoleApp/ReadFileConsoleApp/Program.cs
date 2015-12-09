f052901e15fc47a7eaa7bc9b10e60f1661f00c2f
stefanliden <stefanliden123@hotmail.com>
2015-12-09 17:35:23 +0100

    C# Projekt

---
+++ ReadFileConsoleApp/ReadFileConsoleApp/ReadFileConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ReadFileConsoleApp.localhost1;
+
+
+namespace ReadFileConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+
+            Console.WriteLine("*********************************************");
+            Console.WriteLine("Choose a file to read from below:");
+
+            System.IO.DirectoryInfo dirI = new System.IO.DirectoryInfo(@"C:\test");
+            foreach (var fi in dirI.GetFiles())
+            {
+                Console.WriteLine(fi.Name);
+            }
+            Console.WriteLine("*********************************************");
+
+            answer = Console.ReadLine();
+
+            Console.WriteLine("*********************************************");
+
+            WebService ws = new WebService();
+            string[] lines = ws.filePath(answer);
+
+            Console.WriteLine("Contents of " + answer + ":");
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
